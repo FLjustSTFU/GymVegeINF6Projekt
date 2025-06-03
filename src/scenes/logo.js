653668ad1bbc94a1d879302bf2f55702494d7852
fl <levinvonkauffberg@proton.me>
2025-06-03 19:16:32 +0200

    ready for deployment

--- src/scenes/logo.js
+++ src/scenes/logo.js
@@ -21,7 +21,7 @@ let GBCartridge;
 
 const loader = new GLTFLoader();
 
-loader.load('./assets/GBCartridge.glb',
+loader.load('./models/GBCartridge.glb',
     function (gltf) {
         GBCartridge = gltf.scene;
 
